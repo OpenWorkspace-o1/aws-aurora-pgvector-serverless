c87ed94ff61c892e8c649f284b14dface831dbac
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-01-10 10:33:03 +0700

    Init commit

---
+++ test/aws-aurora-pgvector-serverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsAuroraPgvectorServerless from '../lib/aws-aurora-pgvector-serverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-aurora-pgvector-serverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsAuroraPgvectorServerless.AwsAuroraPgvectorServerlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
