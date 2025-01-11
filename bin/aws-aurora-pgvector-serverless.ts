1e26d2695a5fd4712f8646afa3e58030b1324195
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-01-11 13:54:46 +0700

    Boostrap stack AwsAuroraPgvectorServerlessStack

--- bin/aws-aurora-pgvector-serverless.ts
+++ bin/aws-aurora-pgvector-serverless.ts
@@ -1,20 +1,90 @@
 #!/usr/bin/env node
+import 'source-map-support/register';
+
 import * as cdk from 'aws-cdk-lib';
+import * as dotenv from 'dotenv';
+import { checkEnvVariables } from '../utils/check-environment-variable';
+
+import { ApplyTags } from '../utils/apply-tag';
+import { Aspects } from 'aws-cdk-lib';
+import { AwsSolutionsChecks } from 'cdk-nag';
 import { AwsAuroraPgvectorServerlessStack } from '../lib/aws-aurora-pgvector-serverless-stack';
+import { AuroraEngine, AwsAuroraPgvectorServerlessStackProps } from '../lib/AwsAuroraPgvectorServerlessStackProps';
+import { parseStorageTypeFromEnv } from '../utils/storage-type-parser';
+import { parseClusterScailabilityTypeFromEnv } from '../utils/cluster-scailability-parser';
 
+dotenv.config(); // Load environment variables from .env file
 const app = new cdk.App();
-new AwsAuroraPgvectorServerlessStack(app, 'AwsAuroraPgvectorServerlessStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const appAspects = Aspects.of(app);
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+// check APP_NAME variable
+checkEnvVariables('APP_NAME',
+    'CDK_DEPLOY_REGION',
+    'ENVIRONMENT',
+    'VPC_SUBNET_TYPE',
+    'VPC_PRIVATE_SUBNET_IDS',
+    'VPC_PRIVATE_SUBNET_AZS',
+    'VPC_PRIVATE_SUBNET_ROUTE_TABLE_IDS',
+    'OWNER',
+    'VPC_ID',
+    'AURORA_ENGINE',
+    'SERVERLESS_V2_MAX_CAPACITY',
+    'SERVERLESS_V2_MIN_CAPACITY',
+    'RDS_USERNAME',
+    'RDS_PASSWORD',
+    'DEFAULT_DATABASE_NAME',
+    'STORAGE_TYPE',
+    'MONITORING_INTERVAL',
+    'CLUSTER_SCALABILITY_TYPE',
+);
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const { CDK_DEFAULT_ACCOUNT: account } = process.env;
+
+const cdkRegion = process.env.CDK_DEPLOY_REGION;
+const deployEnvironment = process.env.ENVIRONMENT!;
+
+const appName = process.env.APP_NAME!;
+const owner = process.env.OWNER!;
+
+// check best practices based on AWS Solutions Security Matrix
+appAspects.add(new AwsSolutionsChecks());
+
+appAspects.add(new ApplyTags({
+    environment: deployEnvironment as 'development' | 'staging' | 'production' | 'feature',
+    project: appName,
+    owner: owner,
+}));
+
+const stackProps: AwsAuroraPgvectorServerlessStackProps = {
+    resourcePrefix: `${appName}-${deployEnvironment}`,
+    env: {
+        region: cdkRegion,
+        account,
+    },
+    deployRegion: cdkRegion,
+    deployEnvironment,
+    appName,
+    vpcSubnetType: process.env.VPC_SUBNET_TYPE!,
+    owner,
+    vpcId: process.env.VPC_ID!,
+    vpcPrivateSubnetIds: process.env.VPC_PRIVATE_SUBNET_IDS!.split(','),
+    vpcPrivateSubnetAzs: process.env.VPC_PRIVATE_SUBNET_AZS!.split(','),
+    vpcPrivateSubnetRouteTableIds: process.env.VPC_PRIVATE_SUBNET_ROUTE_TABLE_IDS!.split(','),
+    auroraEngine: process.env.AURORA_ENGINE as AuroraEngine,
+    serverlessV2MaxCapacity: Number(process.env.SERVERLESS_V2_MAX_CAPACITY!),
+    serverlessV2MinCapacity: Number(process.env.SERVERLESS_V2_MIN_CAPACITY!),
+    rdsUsername: process.env.RDS_USERNAME!,
+    rdsPassword: process.env.RDS_PASSWORD!,
+    defaultDatabaseName: process.env.DEFAULT_DATABASE_NAME!,
+    storageType: parseStorageTypeFromEnv(),
+    monitoringInterval: Number(process.env.MONITORING_INTERVAL!),
+    clusterScailabilityType: parseClusterScailabilityTypeFromEnv(),
+};
+new AwsAuroraPgvectorServerlessStack(app, `AwsAuroraPgvectorServerlessStack`, {
+    ...stackProps,
+    stackName: `${appName}-${deployEnvironment}-${cdkRegion}-AwsAuroraPgvectorServerlessStack`,
+    description: `AwsAuroraPgvectorServerlessStack for ${appName} in ${cdkRegion} ${deployEnvironment}.`,
 });
+
+app.synth();
