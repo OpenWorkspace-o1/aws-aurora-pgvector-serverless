f3830e97be166a82341540a0c040c74a9397a718
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-01-17 20:11:55 +0700

    Specify auroraPort

--- lib/constructs/aws-aurora-serverless-nested-stack.ts
+++ lib/constructs/aws-aurora-serverless-nested-stack.ts
@@ -147,6 +147,7 @@ export class AwsAuroraPgvectorServerlessNestedStack extends NestedStack {
             clusterScalabilityType: props.clusterScalabilityType,
             monitoringRole: monitoringRole,
             instanceUpdateBehaviour: rds.InstanceUpdateBehaviour.ROLLING,
+            port: auroraPort,
         });
         this.clusterIdentifier = auroraDatabaseCluster.clusterIdentifier;
 
